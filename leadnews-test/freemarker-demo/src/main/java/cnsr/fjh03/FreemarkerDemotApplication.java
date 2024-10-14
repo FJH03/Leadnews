cad1e1897ef8072d544e1c219a3fd5edd60a5c13
FJH_03 <fengjiahe5@gmail.com>
2024-10-14 16:11:38 +0800

    更新了一个freemarker的demo，方便测试

---
+++ leadnews-test/freemarker-demo/src/main/java/cnsr/fjh03/FreemarkerDemotApplication.java
@@ -0,0 +1,19 @@
+package cnsr.fjh03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Created with Intellij IDEA Ultimate 2022.02.03 正式旗舰版
+ * @Author: 2113042621-冯佳和
+ * @ClassName: FreemarkerDemotApplication
+ * @Date: 2024/10/14
+ * @Time: 16:02
+ * @Description:添加自定义描述
+ */
+@SpringBootApplication
+public class FreemarkerDemotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FreemarkerDemotApplication.class, args);
+    }
+}
