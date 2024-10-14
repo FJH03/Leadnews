ed05f90349303a3c7fe6f7e717e866d5666e4e75
FJH_03 <fengjiahe5@gmail.com>
2024-10-14 17:11:11 +0800

    自媒体内容预处理，配置变更

---
+++ leadnews-gateways/leadnews-wemedia-gateway/src/main/java/cnsr/fjh03/wemedia/gateway/WemediaGatewayApplication.java
@@ -0,0 +1,19 @@
+package cnsr.fjh03.wemedia.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Created with Intellij IDEA Ultimate 2022.02.03 正式旗舰版
+ * @Author: 2113042621-冯佳和
+ * @ClassName: WemediaGatewayApplication
+ * @Date: 2024/10/14
+ * @Time: 16:52
+ * @Description:添加自定义描述
+ */
+@SpringBootApplication
+public class WemediaGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WemediaGatewayApplication.class, args);
+    }
+}
---
+++ leadnews-service/leadnews-wemedia/src/main/java/cnsr/fjh03/wemedia/WemediaApplication.java
@@ -0,0 +1,19 @@
+package cnsr.fjh03.wemedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Created with Intellij IDEA Ultimate 2022.02.03 正式旗舰版
+ * @Author: 2113042621-冯佳和
+ * @ClassName: WemediaApplication
+ * @Date: 2024/10/14
+ * @Time: 16:47
+ * @Description:添加自定义描述
+ */
+@SpringBootApplication
+public class WemediaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WemediaApplication.class, args);
+    }
+}
